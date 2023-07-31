ea8561f74272e0f6c8065febb1c16be44dde2a35
SujitJamdade <jamdadesujit58@gmail.com>
2023-07-31 21:47:23 +0530

    First

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String args[]){
+
+        RWOnly obj = new RWOnly();
+        obj.setName("Sujit");
+        System.out.println(obj.getName());
+    }
   
 }
